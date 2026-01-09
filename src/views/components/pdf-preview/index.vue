ed90b7acf9c42ee0837f1b475f58a50e873de1d7
brose <mcp_zlzp@163.com>
2026-01-09 11:05:06 +0800

    5版本未实现textlayer

--- src/views/components/pdf-preview/index.vue
+++ src/views/components/pdf-preview/index.vue
@@ -1,26 +1,20 @@
 <template>
-       <div class="pdf-viewer relative h-[85vh]" ref="mainContainerRef">
-           
-            </div>
+    <div class="pdf-viewer relative h-[85vh]" ref="mainContainerRef">
+
+    </div>
 </template>
 
 <script setup>
 import { onMounted, ref, watch, unref } from 'vue';
 import * as pdfjsLib from 'pdfjs-dist/legacy/build/pdf';
-import * as pdfJSViewer from 'pdfjs-dist/legacy/web/pdf_viewer';
+import { TextLayerBuilder,EventBus } from 'pdfjs-dist/legacy/web/pdf_viewer';
 import 'pdfjs-dist/web/pdf_viewer.css';
 import pdfWorker from 'pdfjs-dist/legacy/build/pdf.worker?url'
 pdfjsLib.GlobalWorkerOptions.workerSrc = pdfWorker
-const containerRef = ref(null);
 const mainContainerRef = ref(null);
 const totalPages = ref(0);
-const scale = ref(1.4);
-const eventBus = new pdfJSViewer.EventBus();
-const ANNOTATION_MODE = {
-  VIEW: 1,
-  EDIT: 2,
-};
 let pdfInstance = null;
+const eventBus = new EventBus()
 const props = defineProps({
     pdfUrl: {
         type: String,
@@ -32,45 +26,46 @@ const loadPdf = async () => {
     const loadingTask = pdfjsLib.getDocument(props.pdfUrl);
     pdfInstance = await loadingTask.promise;
     totalPages.value = pdfInstance.numPages;
-    let containerOffSetHeight = 0;
-    let containerOffSetWidth = 0;
-    if (mainContainerRef?.value) {
-        containerOffSetHeight = mainContainerRef.value.offsetHeight;
-        containerOffSetWidth = mainContainerRef.value.offsetWidth;
-    }
-    renderPDF({ containerOffSetHeight, containerOffSetWidth });
+    renderPDF();
 };
-const renderPDF = async ({ containerOffSetHeight, containerOffSetWidth }) => {
-        console.log({ containerOffSetHeight, containerOffSetWidth });
+const renderPDF = async () => {
     for (let index = 0; index < totalPages.value; index++) {
         const pageProxy = await pdfInstance.getPage(index + 1);
-        const scaledViewPort = pageProxy.getViewport({ scale: 1});
-        const calculatedScale = Math.min(
-            containerOffSetHeight / scaledViewPort.height,
-            containerOffSetWidth / scaledViewPort.width
-        );
+        const scaledViewPort = pageProxy.getViewport({ scale: 1 });
+        const pageDiv = document.createElement('div');
+        pageDiv.setAttribute('id', `page-${index + 1}`);
+        pageDiv.setAttribute('style', 'position: relative');
 
-        await renderPage({
-            page: pageProxy,
-            pageNumber: index + 1,
-            viewPort: scaledViewPort,
-            scale: calculatedScale,
-        });
+
+        var canvas = document.createElement('canvas');
+        pageDiv.appendChild(canvas);
+        mainContainerRef.value.appendChild(pageDiv);
+        var context = canvas.getContext('2d');
+        canvas.height = scaledViewPort.height;
+        canvas.width = scaledViewPort.width;
+
+        var renderContext = {
+            canvasContext: context,
+            viewport: scaledViewPort
+        };
+
+        await pageProxy.render(renderContext);
+        const textContent = await pageProxy.getTextContent();
+        // // 创建文本图层div
+        const textLayerDiv = document.createElement('div');
+        textLayerDiv.setAttribute('class', 'textLayer');
+        // // 将文本图层div添加至每页pdf的div中
+        pageDiv.appendChild(textLayerDiv);
+         const textLayer = new TextLayerBuilder({
+    textLayerDiv,
+    pageIndex: index + 1,
+    viewport: scaledViewPort,
+    eventBus,
+  })
+
+  textLayer.setTextContent(textContent)
+  await textLayer.render()
     }
-};
-
-const renderPage = async ({ page, pageNumber, viewPort, scale }) => {
-    const pdfPageView = new pdfJSViewer.PDFPageView({
-        container: mainContainerRef.value,
-        id: pageNumber,
-        scale,
-        defaultViewport: viewPort,
-        eventBus,
-        annotationMode: ANNOTATION_MODE.VIEW,
-        // annotationMode: ANNOTATION_MODE.EDIT
-    });
-    pdfPageView.setPdfPage(page);
-    await pdfPageView.draw();
 };
 
 
@@ -123,9 +118,11 @@ onMounted(loadPdf);
         }
     }
 }
-.pdf-viewer{
+
+.pdf-viewer {
     position: relative;
-    .page{
+
+    .page {
         position: relative;
     }
 }
